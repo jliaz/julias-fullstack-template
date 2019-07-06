f6c0ac4da0f17cda0397e76bfce87b7942a64bb1
Julia <juliazhang1108@gmail.com>
2019-07-05 21:17:51 -0400

    client

---
+++ client/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+   <div></div>
+</template>
+<script lang="ts">
+import Vue from "vue";
+import Component from "vue-class-component";
+import * as request from "request-promise-native";
+
+@Component({})
+export default class App extends Vue {
+   constructor() {
+       super();
+       this.test();
+   }
+
+   private async test() {
+       const result = await request.get({ uri: "http://localhost:9000" });
+       console.log(result);
+   }
+}
+
+</script>
+<style scoped>
+</style>
