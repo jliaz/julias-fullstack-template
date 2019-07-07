ed3ec720d24a5aeffcc354198297cdfb07e1e9bb
Julia <juliazhang1108@gmail.com>
2019-07-07 12:18:49 -0400

    parse result

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,6 +1,9 @@
 <template>
    <div>
        <p>Hello World</p>
+       <p> Results: {{ display }} </p>
+       <button v-on:click="test()">Load Results</button>
+       <button v-on:click="first()">Show one entry at a time</button>
    </div>
 </template>
 <script lang="ts">
@@ -13,14 +16,35 @@ export default class App extends Vue {
    constructor() {
        super();
        this.test();
+       
    }
 
+   public display = [];
+   public result = [];
+   public index = 0;
+
    private async test() {
-       const result = await request.get({ uri: "http://localhost:9000" });
-       console.log("result: ", result);
+       this.result = await request.get({ uri: "http://localhost:9000" });
+       console.log("result: ", this.result);
+       this.display = this.result;
+       this.parseResult();
+   }
+
+   private first() {
+       console.log(this.index);
+       if (this.index === this.result.length) {
+           this.index = 0;
+       }
+       this.display = this.result[this.index];
+       this.index++;
+   }
+
+   private parseResult() {
+       this.result = JSON.parse(this.result as any);
    }
 }
 
 </script>
 <style scoped>
 </style>
+ 
